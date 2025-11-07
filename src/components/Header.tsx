91443dd8b4c6a743f38899fdbb1e1ac4d8b4b30c
김소희 <ttohee1275@gmail.com>
2025-11-07 18:58:15 +0900

    feat: 헤더 프로필 클릭 기능개발

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import person_icon from "../assets/icon/person_icon.svg";
+import exit from "../assets/icon/exit.svg";
 import { useNavigate } from "react-router-dom";
 
 interface Prop {
@@ -7,6 +8,7 @@ interface Prop {
 }
 
 const Header = ({ isAuth }: Prop) => {
+  const [clickProfile, setClickProfile] = useState(false);
   const navigate = useNavigate();
   return (
     <div className="w-screen pl-10 pr-14 py-[19px] border-b border-b-mono-4 flex flex-row justify-between items-center">
@@ -14,9 +16,25 @@ const Header = ({ isAuth }: Prop) => {
         우리끼리
       </h1>
       {isAuth && (
-        <div className="w-[52px] h-[52px] rounded-full bg-main-1 flex justify-center items-center">
-          <img src={person_icon} className="w-10 h-10" />
-        </div>
+        <>
+          <div
+            className="w-[52px] h-[52px] relative rounded-full bg-main-1 flex justify-center items-center "
+            onClick={() => setClickProfile(!clickProfile)}
+          >
+            <img src={person_icon} className="w-10 h-10" />
+          </div>
+          {clickProfile && (
+            <div className="absolute right-24 top-16 w-[260px] rounded-[20px] border border-mono-3 overflow-hidden bg-white flex flex-col">
+              <div className="flex justify-center py-5 text-[25px] border-b border-mono-3">
+                마이페이지
+              </div>
+              <div className="flex flex-row gap-2 justify-center py-5 text-[25px] text-system-error">
+                로그아웃
+                <img src={exit} />
+              </div>
+            </div>
+          )}
+        </>
       )}
     </div>
   );
