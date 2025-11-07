690243a0a6e89ed4c1a7939f99f50539b9a6dcff
김소희 <ttohee1275@gmail.com>
2025-11-07 12:18:25 +0900

    feat: 화면 레이아웃 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
+import Header from "./components/Header";
+
+const Layout = () => {
+  return (
+    <>
+      <Header isAuth={true} />
+      <Outlet />
+    </>
+  );
+};
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route />
+        <Route element={<Layout />}></Route>
       </Routes>
     </BrowserRouter>
   );
